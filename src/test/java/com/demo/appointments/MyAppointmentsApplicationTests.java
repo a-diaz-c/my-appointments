5807a85634e91333dcd2565088c6e9613a215bce
Alberto Diaz <diaz1517@hotmail.com>
2025-02-26 13:37:07 -0600

    initial

---
+++ src/test/java/com/demo/appointments/MyAppointmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppointmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
